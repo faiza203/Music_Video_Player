a1b8c341da218a6571f117c002602d325705658f
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-04-23 16:13:40 +0500

    insert play and pause facility

--- script.js
+++ script.js
@@ -5,5 +5,33 @@ const nextButton = document.getElementById("next");
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
 const progressMain = document.getElementById("progress_main");
-const title = document.getElementById('title');
-const cover = document.getElementById('music_image');
+const title = document.getElementById("title");
+const cover = document.getElementById("music_image");
+const music = ["hey", "summer", "ukulele"];
+let musicIndex = 0;
+loadMusic(music[musicIndex]);
+function loadMusic(music) {
+  title.innerText = music;
+  audio.src = `music/${music}.mp3`;
+  cover.src = `image/${music}.png`;
+}
+function playMusic() {
+  musicMain.classList.add("play");
+  playButton.querySelector("i.fa").classList.remove("fa-play");
+  playButton.querySelector("i.fa").classList.add("fa-pause");
+  audio.play();
+}
+function pauseMusic() {
+  musicMain.classList.remove("play");
+  playButton.querySelector("i.fa").classList.add("fa-play");
+  playButton.querySelector("i.fa").classList.remove("fa-pause");
+  audio.pause();
+}
+playButton.addEventListener("click", () => {
+  const isPlaying = musicMain.classList.contains("play");
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+});
